bb15e07d07635fbbc44dfd23134029203fa81e57
王仁义 <tyyn1022@163.com>
2023-01-17 10:22:35 +0800

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,5 +16,8 @@ export default defineNuxtConfig({
         "assets": "/assets"
     },
     modules: [
+    ],
+    css: [
+        "element-plus/dist/index.css"
     ]
 })
--- pages/about.vue
+++ pages/about.vue
@@ -1,22 +1,34 @@
 <template>
   <div>
     这是一个{{ pageName }}
+    <el-button :plain="true" @click="">VNode</el-button>
+    <el-button type="primary">Primary</el-button>
     <NuxtLink to="/">index</NuxtLink>
     <NuxtLink to="about">about</NuxtLink>
     <p v-for="v in list">{{ v.title }}</p>
   </div>
 </template>
+
 <script setup>
-let { data:res } = await useFetch("http://api.oldmedev.top/admin/v1/article/list", {
+import { ElButton } from 'element-plus'
+let pageName = "about"
+
+let { data:d, error:err } = await useFetch("http://api.oldmedev.top/admin/v1/article/list", {
   headers: {
-    "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6MSwiVXNlcm5hbWUiOiJhZG1pbiIsImV4cCI6MTY3Mzc4NjY3MH0.HdfSKJS45v9ELRQYmzUjna4MIkKjmJx70hBy46I__84"
+    "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6MSwiVXNlcm5hbWUiOiJhZG1pbiIsImV4cCI6MTY3Mzg1NDIxNX0.3tKy9zoA5QGPeMeSf-ozxV5rVFauAx_-MIQFcqRl4Zw"
   }
 })
-let list = []
-if (res.value.code !== 0) {
-  console.log(res.value.message)
-} else {
-  list = res.value.data.list
+
+try {
+  if (err.value.statusCode === 200) {
+    let list = []
+    if (d.value.code !== 0) {
+      console.log(d.value.message)
+    } else {
+      list = d.value.data.list
+    }
+  }
+} catch (e) {
+  console.log(e)
 }
 </script>
-
