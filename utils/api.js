0877eeed1e5fb51deb9b4ff9aa24ec2018dde8d7
wangrenyi@traceint.com <wangrenyi@traceint.com>
2023-03-11 23:01:32 +0800

    更换host地址

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 // 开发环境请求地址
 const dev = "http://api.oldmedev.top"
 // 正式环境请求地址
-const pro = "https://api.oldme.top"
+const pro = "https://api.oldme.net"
 
 let api = process.env.NODE_ENV === "development" ? dev : pro
 
