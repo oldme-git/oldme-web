b3749f58dc5bde80d271d9fcad2cc26e1b1a962c
oldme <tyyn1022@gmail.com>
2023-06-01 15:28:59 +0800

    - 优化title和meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,8 +7,8 @@ export default defineNuxtConfig({
                 lang: 'zh-CN'
             },
             charset: "utf-8",
-            viewport: "width=500, initial-scale=1",
-            title: "oldme",
+            viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+            title: "oldme博客 - 请你给我唱首歌吧",
             meta: [
                 { name: "keywords", content: "oldme博客,PHP,Golang,Web开发,Web前端,Vue,Nginx" },
                 { name: "description", content: "oldme的博客，主要用来做IT技术类的知识分享，记录自己的学习历程。主要分享golang，php，vue类的Web知识" },
